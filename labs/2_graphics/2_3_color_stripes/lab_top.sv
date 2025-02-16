2f7e6e79ba5538a3a3fde15459f53cc59d284314
Kiran Kumar.A <kumar.alg2@gmail.com>
2025-02-16 13:51:57 -0800

    Update lab_top.sv spaceship

--- labs/2_graphics/2_3_color_stripes/lab_top.sv
+++ labs/2_graphics/2_3_color_stripes/lab_top.sv
@@ -1,145 +1,92 @@
 `include "config.svh"
 
-module lab_top
-# (
+module lab_top # (
     parameter  clk_mhz       = 50,
                w_key         = 4,
                w_sw          = 8,
                w_led         = 8,
                w_digit       = 8,
                w_gpio        = 100,
-
                screen_width  = 640,
                screen_height = 480,
-
                w_red         = 4,
                w_green       = 4,
                w_blue        = 4,
-
-               w_x           = $clog2 ( screen_width  ),
-               w_y           = $clog2 ( screen_height )
-)
-(
+               w_x           = $clog2(screen_width),
+               w_y           = $clog2(screen_height)
+) (
     input                        clk,
     input                        slow_clk,
     input                        rst,
-
-    // Keys, switches, LEDs
-
     input        [w_key   - 1:0] key,
     input        [w_sw    - 1:0] sw,
     output logic [w_led   - 1:0] led,
-
-    // A dynamic seven-segment display
-
-    output logic [          7:0] abcdefgh,
+    output logic [7:0]           abcdefgh,
     output logic [w_digit - 1:0] digit,
-
-    // Graphics
-
-    input        [w_x     - 1:0] x,
-    input        [w_y     - 1:0] y,
-
+    input        [w_x - 1:0]     x,
+    input        [w_y - 1:0]     y,
     output logic [w_red   - 1:0] red,
     output logic [w_green - 1:0] green,
     output logic [w_blue  - 1:0] blue,
-
-    // Microphone, sound output and UART
-
-    input        [         23:0] mic,
-    output       [         15:0] sound,
-
+    input        [23:0]          mic,
+    output       [15:0]          sound,
     input                        uart_rx,
     output                       uart_tx,
-
-    // General-purpose Input/Output
-
-    inout        [w_gpio  - 1:0] gpio
+    inout        [w_gpio - 1:0]  gpio
 );
 
-    //------------------------------------------------------------------------
-
-       assign led        = '0;
-       assign abcdefgh   = '0;
-       assign digit      = '0;
-    // assign red        = '0;
-    // assign green      = '0;
-    // assign blue       = '0;
-       assign sound      = '0;
-       assign uart_tx    = '1;
-
-    //------------------------------------------------------------------------
+    assign led        = '0;
+    assign abcdefgh   = '0;
+    assign digit      = '0;
+    assign sound      = '0;
+    assign uart_tx    = '1;
 
     wire [3:0] x4;
-
     generate
         if (w_x > 6)
-        begin : wide_x
             assign x4 = x [6:3];
-        end
         else
-        begin
             assign x4 = x;
-        end
     endgenerate
-
-    //------------------------------------------------------------------------
 
     logic [3:0] red_4, green_4, blue_4;
 
-    always_comb
-    begin
+    always_comb begin
         red_4   = '0;
         green_4 = '0;
-        blue_4  = '0;
+        blue_4  = '0';
 
-        // This should be removed after we finish with display_on in wrapper
-        if (x < screen_width && y < screen_height)
-        begin
-
-                 if (x <     128)  red_4   = x4;
-
-            else if (x < 2 * 128)  green_4 = x4;
-
-            else if (x < 3 * 128)  blue_4  = x4;
-
-            else if (x < 4 * 128)
+        if (x < screen_width && y < screen_height) begin
+            // Define spaceship shape
+            if (((x >= 280 && x <= 360) && (y >= 100 && y <= 180)) ||  // Top part of spaceship
+                ((x >= 260 && x <= 380) && (y >= 180 && y <= 220)) ||  // Middle wider body
+                ((x >= 240 && x <= 400) && (y >= 220 && y <= 260)) ||  // Bottom base of spaceship
+                ((x >= 300 && x <= 340) && (y >= 260 && y <= 320)) ||  // Rocket fire exhaust
+                ((x >= 280 && x <= 360) && (y >= 320 && y <= 350)))    // Extended fire trail
             begin
-                red_4   = x4;
-                green_4 = x4;
-            end
-
-            else if (x < 5 * 128)
-            begin
-                green_4 = x4;
-                blue_4  = x4;
+                red_4   = 4'hF;
+                green_4 = 4'h4;
+                blue_4  = 4'h0;
             end
         end
     end
 
     `ifdef VERILATOR
-
         assign red   = w_red'   ( red_4   );
         assign green = w_green' ( green_4 );
         assign blue  = w_blue'  ( blue_4  );
-
     `else
-
         generate
-            if (w_red > 4 & w_green > 4 & w_blue > 4)
-            begin : wide_rgb
+            if (w_red > 4 & w_green > 4 & w_blue > 4) begin
                 assign red   = { red_4   , { w_red   - 4 { 1'b0 } } };
                 assign green = { green_4 , { w_green - 4 { 1'b0 } } };
                 assign blue  = { blue_4  , { w_blue  - 4 { 1'b0 } } };
-            end
-            else
-            begin : narrow_rgb
+            end else begin
                 assign red   = w_red'   ( red_4   );
                 assign green = w_green' ( green_4 );
                 assign blue  = w_blue'  ( blue_4  );
             end
         endgenerate
-
     `endif
 
 endmodule
